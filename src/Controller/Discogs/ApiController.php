93508b69d95e0c8c8c3a77761b44a454266983d9
Radu Maier <ariademur@webserv.co>
2024-10-22 20:38:14 +0200

    createResponseData

--- src/Controller/Discogs/ApiController.php
+++ src/Controller/Discogs/ApiController.php
@@ -7,6 +7,9 @@ namespace App\Controller\Discogs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use UnexpectedValueException;
+
+use function json_encode;
 
 #[Route('/api/discogs')]
 final class ApiController extends AbstractController
@@ -14,6 +17,24 @@ final class ApiController extends AbstractController
     #[Route('/release/{releaseId<\d+>}', name: 'api_discogs_release', methods: ['GET'])]
     public function getRelease(int $releaseId): Response
     {
-        return $this->json(['release' => $releaseId]);
+        return new Response(
+            $this->createResponseData($releaseId),
+            200,
+            [
+                'Content-Type' => 'application/json',
+
+            ],
+        );
+    }
+
+    private function createResponseData(int $releaseId): string
+    {
+        $data = json_encode(['release' => $releaseId]);
+
+        if ($data === false) {
+            throw new UnexpectedValueException('Error converting array to json.');
+        }
+
+        return $data;
     }
 }
